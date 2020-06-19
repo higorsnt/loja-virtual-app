e89c160c0646608327772efd214af392bf3311fa
Higor Santos <higor.dantas@ccc.ufcg.edu.br>
2020-06-19 17:03:42 -0300

    criado area com informação de preço de compra

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,9 @@ import 'package:lojavirtual/models/cart_model.dart';
 import 'package:lojavirtual/models/user_model.dart';
 import 'package:lojavirtual/screens/login_screen.dart';
 import 'package:lojavirtual/tiles/cart_tile.dart';
+import 'package:lojavirtual/widgets/cart_price.dart';
+import 'package:lojavirtual/widgets/discount_card.dart';
+import 'package:lojavirtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -84,9 +87,16 @@ class CartScreen extends StatelessWidget {
             );
           } else {
             return ListView(
-              children: model.products.map((product) {
-                return CartTile(product);
-              }).toList(),
+              children: <Widget>[
+                Column(
+                  children: model.products.map((product) {
+                    return CartTile(product);
+                  }).toList(),
+                ),
+                DiscountCard(),
+                ShipCard(),
+                CartPrice((){}),
+              ],
             );
           }
         },
